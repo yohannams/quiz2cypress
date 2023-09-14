c14da5f5060335c0a097522185e5160fc22dcff9
yohannams <yo.hanna.ms.1993@gmail.com>
2023-09-14 21:42:41 +0700

    register, login

---
+++ cypress/e2e/register.cy.js
@@ -0,0 +1,22 @@
+describe("register", () => {
+  beforeEach(() => {
+    cy.visit("/");
+  });
+
+  it.only("success register", () => {
+    cy.get('[href="/register"]').click();
+    cy.get("#gender-female").check();
+    cy.get("#FirstName").type("Yohanna");
+    cy.get("#LastName").type("Santoso");
+    cy.get("#Email").type("yo_hanna_ms3@yahoo.com");
+    cy.get("#Password").type("123456");
+    cy.get("#ConfirmPassword").type("123456");
+    cy.get("#register-button").click();
+    cy.get(".result").should("contain.text", "Your registration completed");
+    cy.get(".register-continue-button").should("exist").click();
+    cy.get("[href='/customer/info']").should(
+      "contain.text",
+      "yo_hanna_ms3@yahoo.com"
+    );
+  });
+});
---
+++ cypress/support/commands.js
@@ -0,0 +1,30 @@
+// ***********************************************
+// This example commands.js shows you how to
+// create various custom commands and overwrite
+// existing commands.
+//
+// For more comprehensive examples of custom
+// commands please read more here:
+// https://on.cypress.io/custom-commands
+// ***********************************************
+//
+//
+// -- This is a parent command --
+Cypress.Commands.add("login", (username, password) => {
+  cy.get('[href="/login"]').click();
+  cy.get("#Email").type("yo_hanna_ms@yahoo.com");
+  cy.get("#Password").type("123456");
+  cy.get(".login-button").click();
+});
+//
+//
+// -- This is a child command --
+// Cypress.Commands.add('drag', { prevSubject: 'element'}, (subject, options) => { ... })
+//
+//
+// -- This is a dual command --
+// Cypress.Commands.add('dismiss', { prevSubject: 'optional'}, (subject, options) => { ... })
+//
+//
+// -- This will overwrite an existing command --
+// Cypress.Commands.overwrite('visit', (originalFn, url, options) => { ... })
