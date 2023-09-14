31a857c40e11c18b96ce505cfb40197c5f228464
yohannams <yo.hanna.ms.1993@gmail.com>
2023-09-14 22:41:50 +0700

    update workflows

--- cypress.config.js
+++ cypress.config.js
@@ -1,14 +1,3 @@
-// const { defineConfig } = require("cypress");
-
-// module.exports = defineConfig({
-//   e2e: {
-//     baseUrl: "https://demowebshop.tricentis.com/",
-//     setupNodeEvents(on, config) {
-//       // implement node event listeners here
-//     },
-//     defaultCommandTimeout: 5000,
-//   },
-// });
 const { defineConfig } = require("cypress");
 
 module.exports = defineConfig({
