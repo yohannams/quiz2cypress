518e3b7748840a8199f37751e65bd39499a60bac
yohannams <yo.hanna.ms.1993@gmail.com>
2023-09-15 00:09:25 +0700

    test case login, register

--- cypress/e2e/login.cy.js
+++ cypress/e2e/login.cy.js
@@ -8,7 +8,7 @@ describe("login", () => {
     cy.url().should("eq", "https://demowebshop.tricentis.com/");
   });
 
-  it.only("success login", () => {
+  it("success login", () => {
     cy.get('[href="/login"]').click();
     cy.get("#Email").type("yo_hanna_ms@yahoo.com");
     cy.get("#Password").type("123456");
@@ -16,6 +16,40 @@ describe("login", () => {
     cy.get("[href='/customer/info']").should(
       "contain.text",
       "yo_hanna_ms@yahoo.com"
+    );
+  });
+
+  it("success login", () => {
+    cy.get('[href="/login"]').click();
+    cy.get("#Email").type("yo_hanna_ms@yahoo.com");
+    cy.get("#Password").type("123456");
+    cy.get("#RememberMe").check();
+    cy.get(".login-button").click();
+    cy.get("[href='/customer/info']").should(
+      "contain.text",
+      "yo_hanna_ms@yahoo.com"
+    );
+  });
+
+  it("success failed", () => {
+    cy.get('[href="/login"]').click();
+    cy.get("#Email").type("yo_hanna_ms@yahoo.com");
+    cy.get("#Password").type("123");
+    cy.get(".login-button").click();
+    cy.get(".message-error").should("contain.text", "Login was unsuccessful.");
+    cy.get(".message-error").should(
+      "contain.text",
+      "The credentials provided are incorrect"
+    );
+  });
+
+  it("success failed", () => {
+    cy.get('[href="/login"]').click();
+    cy.get(".login-button").click();
+    cy.get(".message-error").should("contain.text", "Login was unsuccessful.");
+    cy.get(".message-error").should(
+      "contain.text",
+      "No customer account found"
     );
   });
 });
--- cypress/e2e/register.cy.js
+++ cypress/e2e/register.cy.js
@@ -3,7 +3,7 @@ describe("register", () => {
     cy.visit("/");
   });
 
-  it.only("success register", () => {
+  it("success register", () => {
     cy.get('[href="/register"]').click();
     cy.get("#gender-female").check();
     cy.get("#FirstName").type("Yohanna");
@@ -17,6 +17,70 @@ describe("register", () => {
     cy.get("[href='/customer/info']").should(
       "contain.text",
       "yo_hanna_ms4@yahoo.com"
+    );
+  });
+
+  it("success register", () => {
+    cy.get('[href="/register"]').click();
+    cy.get("#FirstName").type("Yohanna");
+    cy.get("#LastName").type("Santoso");
+    cy.get("#Email").type("yo_hanna_ms4@yahoo.com");
+    cy.get("#Password").type("123456");
+    cy.get("#ConfirmPassword").type("123456");
+    cy.get("#register-button").click();
+    cy.get(".result").should("contain.text", "Your registration completed");
+    cy.get(".register-continue-button").should("exist").click();
+    cy.get("[href='/customer/info']").should(
+      "contain.text",
+      "yo_hanna_ms4@yahoo.com"
+    );
+  });
+
+  it("failed register", () => {
+    cy.get('[href="/register"]').click();
+    cy.get("#gender-female").check();
+    cy.get("#FirstName").type("Yohanna");
+    cy.get("#Email").type("yo_hanna_ms4@yahoo.com");
+    cy.get("#Password").type("123456");
+    cy.get("#ConfirmPassword").type("123456");
+    cy.get("#register-button").click();
+    cy.get("span[for='LastName']").should(
+      "contain.text",
+      "Last name is required."
+    );
+  });
+
+  it("failed register", () => {
+    cy.get('[href="/register"]').click();
+    cy.get("#gender-female").check();
+    cy.get("#FirstName").type("Yohanna");
+    cy.get("#LastName").type("Santoso");
+    cy.get("#Email").type("yo_hanna_ms4@yahoo.com");
+    cy.get("#Password").type("123456");
+    cy.get("#ConfirmPassword").type("1234567");
+    cy.get("#register-button").click();
+    cy.get("span[for='ConfirmPassword']").should(
+      "contain.text",
+      "The password and confirmation password do not match."
+    );
+  });
+
+  it("failed register", () => {
+    cy.get('[href="/register"]').click();
+    cy.get("#gender-female").check();
+    cy.get("#FirstName").type("Yohanna");
+    cy.get("#LastName").type("Santoso");
+    cy.get("#Email").type("yo_hanna_ms4@yahoo.com");
+    cy.get("#Password").type("123");
+    cy.get("#ConfirmPassword").type("1234567");
+    cy.get("#register-button").click();
+    cy.get("span[for='Password']").should(
+      "contain.text",
+      "The password should have at least 6 characters."
+    );
+    cy.get("span[for='ConfirmPassword']").should(
+      "contain.text",
+      "The password and confirmation password do not match."
     );
   });
 });
